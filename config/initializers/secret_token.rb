26bbd6fb7d0777fbbdb39d64c83552a41fb9fed7
tyap <tyap@blackboard.com>
2013-10-07 20:35:05 -0400

    Add a StaticPages controller

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,4 +1,4 @@
-ure to restart your server when you modify this file.
+# Be sure to restart your server when you modify this file.
 
 # Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
@@ -25,3 +25,4 @@ def secure_token
 end
 
 SampleApp::Application.config.secret_key_base = secure_token
+
